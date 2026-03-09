977a9147124a45d8fa7e189780f409a3cd8b3c5a
Kirima <marid0620915353@gmail.com>
2026-03-10 01:48:55 +0700

    Job Management

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-queue Queue;
+JobQueue Queue;
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
@@ -18,15 +18,16 @@ void callback(char *topic, byte *payload, unsigned int length)
         Serial.print("deserializeJson() failed: ");
         Serial.println(error.c_str());
         return;
-    } 
+    }
     Serial.printf("Converted payload to JSON successfully.\n");
     if (!strcmp(topic, "wirecutter/queue/add"))
     {
         if (doc["command"] == "add_queue")
         {
 
-            Serial.printf("Adding to queue: %s\n", command.c_str());
-            Queue.push_back(command.c_str()[0]);
+            Serial.printf("Adding to queue: %d\n", Queue.get_count());
+            Queue.push_back(doc["total_len"], doc["front_len"], doc["back_len"], doc["quantity"]);
+            Serial.printf("Added to queue. Current count: %d\n", Queue.get_count());
         }
     }
 }
@@ -36,7 +37,7 @@ void setup()
     Serial.begin(115200);
     Serial2.begin(9600, SERIAL_8N1, 18, 17); // RX=18, TX=17
     while (!Serial)
-    Serial.println("--- SYSTEM STARTING ---");
+        Serial.println("--- SYSTEM STARTING ---");
     setup_wifi();
     setup_mqtt(client);
     connect_to_mqtt(client);
@@ -45,9 +46,9 @@ void setup()
 
 void loop()
 {
-    if (!client.connected()) {
+    if (!client.connected())
+    {
         connect_to_mqtt(client);
-
     }
     client.loop();
 
@@ -58,22 +59,16 @@ void loop()
         String resp = Serial2.readStringUntil('\n');
         resp.trim();
         Serial.print("Raw data from Slave: ");
-        Serial.println(resp); // <--- เช็คว่าตรงนี้ขึ้นเลขไหม
+        Serial.println(resp);
 
         if (resp.length() > 0)
         {
             StaticJsonDocument<200> responseDoc;
-
             responseDoc["result"] = resp.toInt();
-
             responseDoc["status"] = "success";
-
             char buffer[256];
-
             serializeJson(responseDoc, buffer);
-
             client.publish("wirecut/topic/status/esp", buffer);
-
             Serial.println("Sent Response back to Node-RED: " + String(buffer));
         }
     }
